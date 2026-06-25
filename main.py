22e4465d700a7402ec26466d74722238301f8d3d
Shaurya Saxena <shauryasaxena836@gmail.com>
2026-06-25 16:25:15 +0530

    changes in file

--- main.py
+++ main.py
@@ -1,18 +1,3 @@
-# ============================================================
-# PROJECT 3: Intraday Strategy Using GARCH Model
-# ============================================================
-# Steps:
-# 1. Load Simulated Daily and Simulated 5-minute data.
-# 2. Fit GARCH model on daily data and predict 1-day ahead volatility.
-# 3. Calculate prediction premium and form a daily signal.
-# 4. Merge with intraday data and calculate intraday indicators.
-# 5. Generate position entry and hold until end of day.
-# 6. Calculate final strategy returns.
-#
-# Required packages:
-# pandas, numpy, matplotlib, arch, pandas_ta, os
-# ============================================================
-
 import matplotlib.pyplot as plt
 import matplotlib.ticker as mtick
 from arch import arch_model
